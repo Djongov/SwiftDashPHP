9bf131037c00b2382d3a59e08822774363ced119
Dimitar Dzhongov <49032971+Djongov@users.noreply.github.com>
2025-03-13 23:18:44 +0200

    fix: session cookie did not start on http://

--- App/Core/Session.php
+++ App/Core/Session.php
@@ -6,16 +6,22 @@ class Session
 {
     public static function start() : void
     {
-        $secure = (str_contains($_SERVER['HTTP_HOST'], 'localhost') || str_contains($_SERVER['HTTP_HOST'], '[::1]')) ? false : true;
+        $secure = (str_contains($_SERVER['HTTP_HOST'], 'localhost') || str_contains($_SERVER['HTTP_HOST'], '[::1]')) 
+              ? false 
+              : (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? true : false);
+        // Session name based on secure connection
         $sesstionName = $secure ? '__Secure-SSID' : 'SSID';
+        // Define the domain based on localhost or actual host
         $domain = (str_contains($_SERVER['HTTP_HOST'], 'localhost') || str_contains($_SERVER['HTTP_HOST'], '[::1]')) ? 'localhost' : $_SERVER['HTTP_HOST'];
+        // Set session name
         session_name($sesstionName);
+        // Set session cookie parameters
         session_set_cookie_params([
-            'lifetime' => 86400,
-            'path' => '/',
-            'domain' => $domain,
-            'secure' => $secure,
-            'httponly' => true,
+            'lifetime' => 86400,  // 1 day
+            'path' => '/',  // Available throughout the site
+            'domain' => $domain,  // Ensure correct domain
+            'secure' => $secure,  // Only secure on HTTPS
+            'httponly' => true,  // Prevent JavaScript access
             'samesite' => ($secure) ? 'None' : 'Lax' // Set to None because of trip to MS Azure AD authentication endpoint and back but None cannot be used with secure false.
         ]);
         session_start();
