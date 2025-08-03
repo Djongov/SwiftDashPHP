fe484dc869541abec209739b5e0c05dfc52450e8
Dimitar Dzhongov <49032971+Djongov@users.noreply.github.com>
2025-08-03 23:58:13 +0300

    Update user.php

--- Views/api/user.php
+++ Views/api/user.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 use Controllers\User;
 use App\Api\Checks;
 use App\Api\Response;
-use App\Exceptions\UserExceptions;
+use App\Authentication\JWT;
+use App\Authentication\AuthToken;
+use App\Logs\SystemLog;
 
 $controller = new User();
 
@@ -34,6 +36,62 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $data['provider'] = 'local'; // Default provider
     $data['enabled'] = true; // Default enabled status
     $controller->create($data, 'local');
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'PUT') {
+    $data = Checks::jsonBody();
+    if (!isset($routeInfo[2]['id'])) {
+        Response::output('Missing user id', 400);
+        exit();
+    }
+
+    $userId = (int) $routeInfo[2]['id'];
+
+    $checks = new Checks($loginInfo, $data);
+    $checks->apiChecks();
+
+    $user = new User();
+    $dbUserData = $user->get($userId);
+
+    $tokenData = JWT::parseTokenPayLoad(AuthToken::get());
+    $dbUserDataFromToken = $tokenData['preferred_username'] ?? $tokenData['username'] ?? $tokenData['email'];
+
+    if ($dbUserData['username'] !== $dbUserDataFromToken) {
+        Response::output('You cannot edit another user data', 401);
+    }
+
+    if (isset($data['password']) && isset($data['confirm_password']) && $data['password'] !== $data['confirm_password']) {
+        Response::output('Passwords do not match', 400);
+    }
+
+    if (isset($data['password'])) {
+        $data['password'] = password_hash($data['password'], PASSWORD_DEFAULT);
+    }
+
+    if (isset($data['role']) && !in_array('administrator', $tokenData['roles'])) {
+        Response::output('Only administrators can change roles', 401);
+    }
+
+    unset($data['confirm_password'], $data['csrf_token'], $data['username']);
+
+    if (isset($data['picture']) && empty($data['picture'])) {
+        $currentPicture = $user->get($userId)['picture'];
+        $profilePicturePath = ROOT . '/public' . $currentPicture;
+        if (file_exists($profilePicturePath)) {
+            unlink($profilePicturePath);
+        } else {
+            SystemLog::write('Could not delete the picture: ' . $profilePicturePath . '. Full payload was ' . json_encode($data), 'error');
+        }
+    }
+    try {
+        $rowCount = $user->update($data, $userId);
+        if ($rowCount === 0) {
+            Response::output('user not updated', 400);
+        }
+        Response::output('user updated', 200);
+    } catch (\Throwable $e) {
+        Response::output('Invalid field: ' . $e->getMessage(), 400);
+    }
 }
 
 // Handle DELETE requests
